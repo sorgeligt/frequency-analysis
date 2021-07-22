de8c77c237cd17a05f82a85d8863a723234d0f34
sorgeligt <ubutno@gmail.com>
2021-07-22 13:34:20 +0300

    Init

--- main.cpp
+++ main.cpp
@@ -1,310 +1,165 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
+#include <malloc.h>
 #include <stdlib.h>
+#include <locale.h>
+#include <windows.h>
 
-#define N 10000000
+struct History { // композитный тип данных для реализации истории
+    int index[10000], j; // индексы замен в файле и размер массива
+    char prev, now;
+};
 
-const int bs = 1000000000;
-int size_num1, size_num2, sign, num1[N], num2[N], tmp_zero1[N] = {1, 0}, tmp_zero2[N] = {1, 0};
-int num1_cpy[N], num_sum[N], ans_fac[N] = {1,1}, array1[N] = {1,1}, array1_2[2] = {1,1};
-long long ans[N];
-char sign1 = '+', sign2 = '+';
+struct Symb_info { // композитный тип данных для реализации частотного анализа
+    char symbol, change;
+    int count;
+};
 
-int max(int a, int b){
-    return a > b ? a : b;
+int global_size = 0, count_dif_symb = 0, history_top = 0;
+char spectrum[] = "оеаинтрслвкпмудяыьзбгйчюхжшцщфэъ", alphabet[] = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+struct Symb_info symb[32];
+struct History list[35];
+
+int is_decode(char c) { // проверка на заглавные буквы
+    return ((c >= 'А') && (c <= 'Я'));
 }
 
-int min(int a, int b){
-    return a < b ? a : b;
+int is_letter(char c) { // проверка на буквы русского языка
+    return (((c >= 'А') && (c <= 'Я')) || ((c >= 'а') && (c <= 'я')));
 }
 
-void scan_sign(){
-    printf("Please, choose set of signs for your numbers:\n1: + +\n2: + -\n3: - +\n4: - -\n");
-    scanf("%d", &sign);
-    if(sign == 2) sign2 = '-';
-    if(sign == 3) sign1 = '-';
-    if(sign == 4){
-        sign1 = '-';
-        sign2 = '-';
+int cmp(const void* a, const void* b) { // компаратор
+    const struct Symb_info* A = (const struct Symb_info*)a;
+    const struct Symb_info* B = (const struct Symb_info*)b;
+    if (A->count < B->count) return 1;
+    else if (A->count > B->count) return -1;
+    else return 0;
+}
+
+void analysis(struct Symb_info* symb, char* p) {
+    for (int i = 0; i < 32; i++) { // начальная инициализация структуры
+        symb[i].symbol = alphabet[i];
+        symb[i].count = 0;
     }
-}
-int cmp_big(int first[], int second[]){
-    if(first[0] == second[0] && first[first[0]] == second[second[0]]) return 0;
-    if(first[0] == second[0]  && first[first[0]] > second[second[0]]) return 1;
-    if(first[0] == second[0]  && first[first[0]] < second[second[0]])  return -1;
-    if(first[0] > second[0]) return 1;
-    if(second[0] > first[0]) return -1;
-    printf("ERROR_");
-    return 0;
+    for (int i = 0; i < global_size; i++) // подсчет встречающихся букв
+        if (is_decode(p[i])) symb[p[i] - 'А'].count++;
+    qsort(symb, 32, sizeof(struct Symb_info), cmp); // сортировка по полю count
+    for (int i = 0; i < 32 && symb[i].count != 0; ++i) count_dif_symb = i + 1; // кол-во различных чисел в файле
+    for (int i = 0; i < count_dif_symb; ++i) symb[i].change = spectrum[i]; // соответвие букв в соответствии с частотным анализом
 }
 
-void num_scan(char str_num[1001], int num[N], int* len){
-    scanf("%s", str_num);
-    *len = strlen(str_num);
-    num[0] = ((*len) - 1) / 9 + 1;
-    for(int i = 1; i <= num[0]; ++i) {
-        for(int j = 9; j >= 1; --j) {
-            int point = (*len) - (i - 1) * 9 - j;
-            if (point >= 0) {
-                num[i] = num[i] * 10 + (str_num[point] - 48);
-            }
+void auto_decode(struct Symb_info* symb, char* p) { // автоматическая расшифровка
+    for (int i = 0; i < global_size; i++)
+        for (int j = 0; j < count_dif_symb; ++j)
+            if (symb[j].symbol == p[i]) p[i] = symb[j].change;
+}
+
+void analysis_print(struct Symb_info* symb) { // вывод анализа
+    for (int i = 0; i < count_dif_symb; ++i)
+        printf("%lf: %c -> %c\n", symb[i].count * 1.0 / (global_size - 1), symb[i].symbol, symb[i].change);
+}
+
+void selection_size_n(char* p, char check) { // check: 'a' = all, 'd' = decode
+    int cnt=0, n, move=0, j; //move: при подсчете нерасшифрованных символов отвечает за кол-во маленьких букв
+    printf("Необходимое кол-во символов: ");
+    scanf("%d", &n);
+    for (int i = 0; i < global_size; i++) {
+        if (check == 'd' && is_letter(p[i]) && !is_decode(p[i])) {
+            move++; continue; // пропускаем маленькие буквы при подсчете нерасшифрованных
+        }
+        if (check == 'a' ? is_letter(p[i]) : is_decode(p[i])) cnt++;
+        else if ((cnt == n && move != 0) || (cnt == n && n != 0)) { // при достижении нужной длины выводим ответ с учетом 2 вариантов работы функции
+            for (j = i - cnt - move; j < i; j++) printf("%c", p[j]);
+            printf("\n"); cnt = 0; move = 0;
+        }
+        else {
+            cnt = 0; move = 0;
         }
     }
-
 }
 
-void num_print(int num[]){
-    printf ("%d", num[num[0]]);
-    for (int i=num[0] - 1; i > 0; --i)
-        printf ("%.9d", num[i]);
+void crypto_print(char* p) { // вывод криптограммы
+    for (int i = 0; i < global_size; i++) printf("%c", p[i]);
+    printf("\n");
 }
 
-void addition(int first[], int second[]) {
-    if (first[0] < second[0])
-        first[0] = second[0];
-    int move = 0;
-    for (int i = 1; i <= first[0]; ++i) {
-        first[i] += second[i] + move;
-        if (first[i] >= bs) {
-            move = 1;
-            first[i] -= bs;
-        }
-        else
-            move = 0;
+void replace_letters(char a, char b, char* p, struct History *list) {
+    if (!is_decode(a) || is_decode(b) || !is_letter(b) || !is_letter(a)) {
+        printf("Ошибка! Введите 1 символ заглавным, а 2 строчным вводами\nПример:Ы и\n");
+        return;
     }
-    if (move) {
-        first[0]++;
-        first[first[0]] = 1;
-    }
-}
-
-void subtract(int first[], int second[]) {
-    int move = 0;
-    for (int i = 1; i <= first[0]; ++i) {
-        first[i] -= second[i] + move;
-        if (first[i] < 0) {
-            move = 1;
-            first[i] += bs;
-        }
-        else
-            move = 0;
-    }
-    while (first[0] > 1 && first[first[0]] == 0) {
-        first[0]--;
-    }
-}
-
-void multiplication(int first[], int second[]) {
-    memset(ans, 0, (first[0]+second[0]+1)*8);
-    for (int i = 1; i <= first[0]; ++i) {
-        long long move = 0;
-        for (int j = 1; j <= second[0] || move>0; ++j) {
-            ans[i + j - 1] += (long long)first[i] * (long long)second[j] + move;
-            move = (long long)ans[i + j - 1] / (long long)bs;
-            ans[i + j - 1] -= (long long)move * (long long)bs;
-
+    int j = 0;
+    for (int i = 0; i < global_size; i++){
+        if (a == p[i] && is_decode(p[i])) { // меняем нерасшифрованные символы и заносим изменения в историю
+            list[history_top].prev = a;
+            list[history_top].now = p[i] = b;
+            list[history_top].index[j++] = i;
         }
     }
-    ans[0] = first[0] + second[0];
-    for (int i = 0; i <= ans[0]; ++i) first[i] = ans[i];
-    while (first[0] > 1 && first[first[0]] == 0) {
-        first[0]--;
-    }
+    list[history_top++].j = j; // обновляем размер массива отвечающего за производимые изменения на данном шаге
 }
 
-void power(int first[], int second[]) {
-    memset(num1_cpy, 0, sizeof(num1_cpy));
-    memccpy(num1_cpy, first, -1, sizeof(int) * N);
-    subtract(second, array1);
-    while (second[1] != 0){
-        subtract(second, array1);
-        multiplication(first, num1_cpy);
-    }
+void hitory_list(struct History* list) { // вывод истории
+    for (int i = 0; i < history_top; i++) printf("%c -> %c\n", list[i].prev, list[i].now);
 }
 
-void factorial(int first[]) {
-    while(array1[first[0]] != first[first[0]]){
-        addition(array1, array1_2);
-        multiplication(ans_fac, array1);
+void undo(char* p, struct History* list) { // откат
+    if (history_top == 0) {
+        printf("Ошибка! История пуста\n");
+        return;
     }
-    memmove(first, ans_fac, sizeof(ans_fac));
-
+    for (int i = 0; i < list[history_top-1].j; i++) p[list[history_top-1].index[i]] = list[history_top-1].prev;
+    list[history_top--].j = 0;
 }
 
-void sum(int first[], int second[]){
-    memmove(num_sum, first, sizeof(num1));
-    while (first[first[0]] !=  second[second[0]] || first[0] != second[0]){
-        addition(first, array1);
-        addition(num_sum, first);
-    }
-    memmove(first, num_sum, sizeof(num_sum));
+int user_communication() { // интерфейс взаимодействия с пользвателем
+    int n;
+    printf("Выберите:\n1) Анализ\n2) Авторасшифровка\n3) Слова по кол-ву букв\n4) Слова по кол-ву нерасшифрованных букв\n");
+    printf("5) Отображение криптограммы\n6) Замена букв\n7) История\n8) Отмена последней замены\n9) Выход\n");
+    scanf("%d", &n);
+    return n;
 }
 
 int main() {
-    char str_num1[1001], str_num2[1001];
-
-    int op, check;
-    printf("Input number of mathematical symbol (1 = '+', 2 = '-', 3 = '*', 4 = '^', 5 = '!', 6 = 'sum[a, b]'):\n");
-    scanf("%d", &op);
-    if (op == 1) {
-        scan_sign();
-        printf("Input numbers:\n");
-        num_scan(str_num1, num1, &size_num1);
-        num_scan(str_num2, num2, &size_num2);
-        int check = cmp_big(num1, num2);
-        if(sign1 == '+' && sign2 == '+'){
-            addition(num1, num2);
-            num_print(num1);
-        }
-        else if (sign1 == '+' && sign2 == '-'){
-            if (check >= 0){
-                subtract(num1, num2);
-                num_print(num1);
-            }
-            else if (check < 0){
-                subtract(num2, num1);
-                printf("-");
-                num_print(num2);
-            }
-        }
-        else if (sign1 == '-' && sign2 == '+') {
-            int check = cmp_big(num1, num2);
-            if (check > 0){
-                subtract(num1, num2);
-                printf("-");
-                num_print(num1);
-            }
-            else if (check <= 0){
-                subtract(num2, num1);
-                num_print(num2);
-            }
-        }
-        else if (sign1 == '-' && sign2 == '-'){
-            addition(num1, num2);
-            printf("-");
-            num_print(num1);
-        }
-        else printf("ERROR");
+    SetConsoleCP(1251); // установка win-cp 1251 в поток ввода
+    SetConsoleOutputCP(1251); // установка win-cp 1251 в поток вывода
+    setlocale(LC_ALL, "Russian"); // задаем локаль используемую программой
+    FILE* f = fopen("lab_var_5.txt", "r");
+    if (f == NULL) {
+        printf("Ошибка! Такого файла нет в директории");
+        return 0;
     }
-    if (op == 2) {
-        scan_sign();
-        printf("Input numbers:\n");
-        num_scan(str_num1, num1, &size_num1);
-        num_scan(str_num2, num2, &size_num2);
-        int check = cmp_big(num1, num2);
-        if(sign1 == '+' && sign2 == '+'){
-            if (check >= 0){
-                subtract(num1, num2);
-                num_print(num1);
-            }
-            else if (check < 0){
-                subtract(num2, num1);
-                printf("-");
-                num_print(num2);
-            }
-        }
-        else if (sign1 == '+' && sign2 == '-'){
-            addition(num1, num2);
-            num_print(num1);
-        }
-        else if (sign1 == '-' && sign2 == '+') {
-            addition(num1, num2);
-            printf("-");
-            num_print(num1);
-
-        }
-        else if (sign1 == '-' && sign2 == '-'){
-            if (check > 0){
-                subtract(num1, num2);
-                printf("-");
-                num_print(num1);
-            }
-            else if (check <= 0){
-                subtract(num2, num1);
-                num_print(num2);
-            }
-        }
-        else printf("ERROR");
+    char *p, c;
+    p = (char*)malloc(sizeof(char)); // выделяем блок памяти для массива символов
+    while (fscanf(f, "%c", &c) != EOF) { // считываем символы с файла занося в массив
+        p[global_size++] = c;
+        p = (char*)realloc(p, sizeof(char) * (global_size + 5));
     }
-    if (op == 3) {
-        scan_sign();
-        printf("Input numbers:\n");
-        num_scan(str_num1, num1, &size_num1);
-        num_scan(str_num2, num2, &size_num2);
-        int check = cmp_big(num1, num2);
-        if (sign1 == sign2){
-            if (check) {
-                multiplication(num1, num2);
-                num_print(num1);
-            }
-            else{
-                multiplication(num2, num1);
-                num_print(num2);
-            }
-        }
-        else{
-            if(check) {
-                multiplication(num1, num2);
-                num_print(num1);
-            }
-            else {
-                multiplication(num2, num1);
-                printf("-");
-                num_print(num2);
-            }
-        }
+    if (global_size == 0) {
+        printf("Ошибка! Файл пуст");
+        return 0;
     }
-    if (op == 4) {
-        printf("Input numbers without signs:\n");
-        num_scan(str_num1, num1, &size_num1);
-        num_scan(str_num2, num2, &size_num2);
-        power(num1, num2);
-        num_print(num1);
+    p[global_size++] = '\0';
+    analysis(symb, p); // предварительные инициализации и анализ файла
+    int step = user_communication();
+    while (step != 9) { // интерфейс взаимодействия
+        if (step == 1) analysis_print(symb);
+        if (step == 2) auto_decode(symb, p);
+        if (step == 3) selection_size_n(p, 'a');
+        if (step == 4) selection_size_n(p, 'd');
+        if (step == 5) crypto_print(p);
+        if (step == 6) {
+            char a[10], b[10];
+            printf("Введите две буквы через пробел:"); scanf("%s %s", a, b);
+            replace_letters(a[0], b[0], p, list);
+        }
+        if (step == 7) hitory_list(list);
+        if (step == 8) undo(p, list);
+        if (step == 9) {
+            free(p); return 0;
+        }
+        step = user_communication();
     }
-    if (op == 5) {
-        printf("Input number without sign:\n");
-        num_scan(str_num1, num1, &size_num1);
-        factorial(num1);
-        num_print(num1);
-    }
-    if (op == 6){
-        printf("Please, choose set of signs for your numbers:\n1: + +\n2: - +\n3: - -\n");
-        scanf("%d", &sign);
-        if(sign == 2) sign1 = '-';
-        if(sign == 3){
-            sign1 = '-';
-            sign2 = '-';
-        }
-        printf("Input numbers:\n");
-        num_scan(str_num1, num1, &size_num1);
-        num_scan(str_num2, num2, &size_num2);
-        if (sign1 == '-' && sign2 == '+'){
-            sum(tmp_zero1, num1);
-            sum(tmp_zero2, num2);
-            int check = cmp_big(tmp_zero1, tmp_zero2);
-            if (check == 0){
-                printf("0");
-            }
-            else if (check > 0){
-                subtract(tmp_zero1, tmp_zero2);
-                printf("-");
-                num_print(tmp_zero1);
-            }
-            else if (check < 0){
-                subtract(tmp_zero2, tmp_zero1);
-                num_print(tmp_zero2);
-            }
-        }
-        else if (sign1 == '-' && sign2 == '-'){
-            sum(num2, num1);
-            printf("-");
-            num_print(num2);
-        }
-        else if(sign1 == '+' && sign2 == '+'){
-            sum(num1, num2);
-            num_print(num1);
-        }
-    }
+    free(p); // освобождение памяти
     return 0;
 }
